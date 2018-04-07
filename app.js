2bece6b1f070cc6dd945e8710d95bd42f9d059a5
uemurar51 <uemurar51@gmail.com>
2018-04-07 12:04:07 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-var express = require('express');
-var app = express();
+//====LIST DEPENDENCIES===//
+const express = require('express');
+const parseurl = require('parseurl');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+//const Signature = require('./models/signature.js');
+const app = express();
+//const url = 'mongodb://localhost:27017/signatures';
+//=========================//
 
+//app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	console.log("we at the hompage bro");
+	res.send("hello world");
+});
+
+app.listen(3000, function(){
+   console.log("Server is listening!!!"); 
+});
