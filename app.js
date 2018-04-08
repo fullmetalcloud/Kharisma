61fb50da7519a3c42f164b0fd223cda24ad8bdca
fullmetalcloud <gueved90@gmail.com>
2018-04-08 13:55:31 -0700

    working app

--- app.js
+++ app.js
@@ -14,24 +14,27 @@ const match = require('./match.js');
 const uri = "mongodb://admin:hihihi@kharismacluster-shard-00-00-vocgi.mongodb.net:27017,kharismacluster-shard-00-01-vocgi.mongodb.net:27017,kharismacluster-shard-00-02-vocgi.mongodb.net:27017/Classes?ssl=true&replicaSet=KharismaCluster-shard-0&authSource=admin";
 //=========================//
 
-router.post('/api', (req, res) => {
-	// let attributes = {
-	// 	"Str": req.body.Str,
-	// 	"Dex": req.body.Dex,
-	// 	"Con": req.body.Con,
-	// 	"Int": req.body.Int,
-	// 	"Wis": req.body.Wis,
-	// 	"Cha": req.body.Cha
-	// };
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // handle URL-encoded data
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+router.post('/api', (req, res) => {
+	console.log(req);
 	let attributes = {
-		"Str": 5,
-		"Dex": 4,
-		"Con": 3,
-		"Int": 2,
-		"Wis": 1,
-		"Cha": 0
+		"Str": req.body.Str,
+		"Dex": req.body.Dex,
+		"Con": req.body.Con,
+		"Int": req.body.Int,
+		"Wis": req.body.Wis,
+		"Cha": req.body.Cha
 	};
+
+
 	//open the connection to the database
 	mongoose.Promise = global.Promise;
 	mongoose.connect(uri);
