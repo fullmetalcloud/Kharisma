99f71309b5189a40e6d7b863beb329d7d693c939
fullmetalcloud <gueved90@gmail.com>
2018-04-07 18:18:12 -0700

    match.js added

--- app.js
+++ app.js
@@ -10,8 +10,35 @@ const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
 const Classes = require('./models/model');
+const match = require('./match.js');
 const uri = "mongodb://admin:hihihi@kharismacluster-shard-00-00-vocgi.mongodb.net:27017,kharismacluster-shard-00-01-vocgi.mongodb.net:27017,kharismacluster-shard-00-02-vocgi.mongodb.net:27017/Classes?ssl=true&replicaSet=KharismaCluster-shard-0&authSource=admin";
 //=========================//
+
+let attributes = {
+	"Str": 5,
+	"Dex": 4,
+	"Con": 3,
+	"Int": 2,
+	"Wis": 1,
+	"Cha": 0
+};
+//open the connection to the database
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  	// we're connected!!
+  	// find each person with a last name matching 'Ghost', selecting the `name` and `occupation` fields
+	Classes.find({}, (err, classes) => {
+		let results = match.determinecharacters(attributes, classes);
+		console.log(results);
+		// res.json(results);
+	});
+});
+
+
+
 
 //app.set("view engine", "ejs");
 
@@ -21,15 +48,23 @@ router.get('/', (req, res) => {
 });
 
 router.post('/api', (req, res) => {
-	/*let attributes = {
-		"str": req.body.Str,
-		"dex": req.body.Dex,
-		"con": req.body.Con,
-		"int": req.body.Int,
-		"wis": req.body.Wis,
-		"cha": req.body.Cha
-	};*/
+	// let attributes = {
+	// 	"Str": req.body.Str,
+	// 	"Dex": req.body.Dex,
+	// 	"Con": req.body.Con,
+	// 	"Int": req.body.Int,
+	// 	"Wis": req.body.Wis,
+	// 	"Cha": req.body.Cha
+	// };
 
+	let attributes = {
+		"Str": 5,
+		"Dex": 4,
+		"Con": 3,
+		"Int": 2,
+		"Wis": 1,
+		"Cha": 0
+	};
 	//open the connection to the database
 	mongoose.Promise = global.Promise;
 	mongoose.connect(uri);
@@ -39,8 +74,9 @@ router.post('/api', (req, res) => {
 	  	// we're connected!!
 	  	// find each person with a last name matching 'Ghost', selecting the `name` and `occupation` fields
 		Classes.find({}, (err, classes) => {
-
-			res.json(classes);
+			var results = match.determinecharacters(attributes, classes);
+			console.log(results);
+			res.json(results);
 		});
 	});
 });
---
+++ match.js
@@ -0,0 +1,55 @@
+var f = function determinecharacters(values, classes) {
+	let primary = "",
+		secondary = "",
+		primaryVal = 0,
+		secondaryVal = 0, 
+		list = [];
+
+	// find two highest attributes
+	for (key in values) {
+		value = values[key];
+		if (value > primaryVal) {
+			secondary = primary;
+			secondaryVal = primaryVal;
+			primary = key;
+			primaryVal = value;
+		}
+		else if (value > secondary) {
+			secondary = key;
+			secondaryVal = value;
+		}
+	}
+	for (let i in classes) {
+		dndclass = classes[i];
+		if(dndclass.Primary === primary && list.length < 5) {
+			list.push(dndclass);
+		}
+	}
+	if(list.length < 5) {
+		for (let i in classes) {
+			dndclass = classes[i];
+			if(dndclass.Secondary === primary && !list.includes(dndclass) && list.length < 5) {
+				list.push(dndclass);
+			}
+		}
+	}
+	if(list.length < 5) {
+		for (let i in classes) {
+			dndclass = classes[i];
+			if(dndclass.Primary === secondary && !list.includes(dndclass) && list.length < 5) {
+				list.push(dndclass);
+			}
+		}
+	}
+	if(list.length < 5) {
+		for (let i in classes) {
+			dndclass = classes[i];
+			if(dndclass.Secondary === secondary && !list.includes(dndclass) && list.length < 5) {
+				list.push(dndclass);
+			}
+		}
+	}
+	return list;
+}
+
+module.exports = {determinecharacters: f};
